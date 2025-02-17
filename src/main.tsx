7e0381ffcb81c8793942ad77037f034798ba70e2
Sheroz Turdiyev <sherozturdiyev95@gmail.com>
2025-02-17 20:23:28 +0500

    added react-router-dom bollar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+  import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<h1>Index Page</h1>} />
+            <Route path="/main" element={<h1>Index Page</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
